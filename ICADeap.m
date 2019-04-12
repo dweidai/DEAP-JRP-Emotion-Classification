45f4c0ad6da4051cabff0a92c8097980a545af3a
David Dai <daiwei19971009@gmail.com>
2019-04-12 16:09:11 -0700

    Add files via upload

---
+++ ICADeap.m
@@ -0,0 +1,21 @@
+filespath = dir('/Users/apple/Desktop/eeglab14_1_2b/DEAP/*.mat');
+EEG = eeglab();
+DEAP = 'DEAP ';
+ICA = ' ICA';
+save_dir = '/Users/apple/Desktop/eeglab14_1_2b/DEAP_ICA/';
+post_filename = '.set';
+for k = 1:(numel(filespath))
+    Directory = '/Users/apple/Desktop/eeglab14_1_2b/DEAP/';
+    filename = strcat(Directory, filespath(k).name)
+    EEG = pop_importdata('dataformat','matlab','nbchan',32,'data',filename,'srate',128,'pnts',0,'xmin',0);
+    num = num2str(k)
+    cur_setname = strcat(DEAP, num, ICA);
+    EEG.setname= cur_setname;
+    EEG = eeg_checkset( EEG );
+    EEG = eeg_checkset( EEG );
+    EEG = pop_runica(EEG, 'extended',1,'interupt','on','pca',31);
+    EEG = eeg_checkset( EEG );
+    save_name = strcat(DEAP, num, post_filename);
+    EEG = pop_saveset( EEG, 'filename', save_name,'filepath',save_dir);
+    EEG = eeg_checkset( EEG );
+end;

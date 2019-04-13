6565a91a96ff92d7e6e2aa20d1709ae191d43df0
David Dai <daiwei19971009@gmail.com>
2019-04-13 10:46:31 -0700

    Update ICADeap.m

--- ICADeap.m
+++ ICADeap.m
@@ -1,3 +1,34 @@
+%filespath = dir('/Users/apple/Desktop/eeglab14_1_2b/data_preprocessed_matlab/*.mat');
+% the eeglab channel * data * trials
+% deap is trials * channel * data
+Directory = '/Users/apple/Desktop/eeglab14_1_2b/data_preprocessed_matlab/';
+filename = strcat(Directory, filespath(1).name)
+S = load(filename);
+for k = 2:(numel(filespath))
+    filename = strcat(Directory, filespath(k).name)
+    S(k) = load(filename);
+end
+pre_filename = '/Users/apple/Desktop/eeglab14_1_2b/data_preprocessed_matlab/DEAP'
+num = num2str(i)
+post_filename = '.mat'
+unused = 8;
+[x, y, z] = size(S(1).data);
+y = y - unused; %remove the unused channels
+Channel = S(1).data(:,[1:y],:);
+Channel = permute(Channel, [2,3,1]);
+new_filename = strcat(pre_filename, num, post_filename)
+save(new_filename,'Channel')
+for i = 2:length(S)
+    num = num2str(i)
+    [x, y, z] = size(S(i).data);
+    y = y - unused; %remove the unused channels
+    Channel = S(i).data(:,[1:y],:);
+    Channel = permute(Channel, [2,3,1]);
+    new_filename = strcat(pre_filename, num, post_filename)
+    save(new_filename,'Channel')
+end
+%should be 32 * 8064 * 1280
+
 filespath = dir('/Users/apple/Desktop/eeglab14_1_2b/DEAP/*.mat');
 EEG = eeglab();
 DEAP = 'DEAP ';
@@ -8,7 +39,7 @@ for k = 1:(numel(filespath))
     Directory = '/Users/apple/Desktop/eeglab14_1_2b/DEAP/';
     filename = strcat(Directory, filespath(k).name)
     EEG = pop_importdata('dataformat','matlab','nbchan',32,'data',filename,'srate',128,'pnts',0,'xmin',0);
-    num = num2str(k)
+    num = num2str(k)dun
     cur_setname = strcat(DEAP, num, ICA);
     EEG.setname= cur_setname;
     EEG = eeg_checkset( EEG );
